7b63c3e70664597624084f68f28939d937d200d7
Bart Kalanski <bkalanski@gmail.com>
2020-10-19 19:18:11 +0100

    Abstracted API call into the Skyscanner module

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "./Logo/Logo";
 import SearchBar from "./SearchBar";
 import FlightResults from "./FlightResults";
+import Skyscanner from "./Skyscanner";
 import date from "./Utilities/date";
 import axios from "axios";
 
 const App = () => {
   const [results, setResults] = useState("");
+
   const onSearchSubmit = async ({ from, to, depart, back }) => {
-    const { data } = await axios.get(
-      `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/UK/GBP/en-GB/${from}/${to}/${date(
-        depart
-      )}/${date(back)}`,
-      {
-        headers: {
-          "x-rapidapi-host":
-            "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-          "x-rapidapi-key":
-            "10519e3170mshfad2e75acb40046p1fdba8jsnca5e5cac79cd",
-        },
-      }
+    const { data } = await Skyscanner.get(
+      `${from}/${to}/${date(depart)}/${date(back)}`
     );
     setResults(data);
   };
