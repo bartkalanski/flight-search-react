d9fed18bdf327c2e1c3174a79704874bbc010bfe
Bart Kalanski <bkalanski@gmail.com>
2020-10-25 22:07:47 +0000

    Added font awesome icons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -72,7 +72,7 @@ const SearchBar = ({ onSubmit }) => {
             />
           </div>
         </div>
-        <button className="search-bar__btn">Search Flights</button>
+        <button className="search-bar__btn">Search Flights<i class="fas fa-arrow-right"></i></button>
       </form>
     </div>
   );
