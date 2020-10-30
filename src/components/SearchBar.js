fe7a2c409ba86a2c02cc5cb633a3bf1ad7c77204
Rob Kendal <38687284+bpk68@users.noreply.github.com>
2020-10-30 19:40:46 +0000

    Refactors Input components for useRef hook

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,51 +1,81 @@
 import React, { useState, useEffect } from "react";
+import airportAutcomopleteJs from "airport-autocomplete-js";
+import { Helmet } from "react-helmet";
 import Input from "./Input";
 
 import "./SearchBar.css";
 
 const SearchBar = ({ onSubmit }) => {
   const [formValues, setFormValues] = useState({});
-  const [refValues, setRefValues] = useState({});
 
-  const handleFormValuesChange = (name, value, ref) => {
-    setRefValues({
-      ...refValues,
-      [name]: ref.current.dataset, //ref.current.dataset.iata
-    });
+  const [inputRefs, setInputRefs] = useState({});
+
+  const registerRef = (name, ref) => {
+    //console.log('ref changed', ref);
+    setInputRefs({
+      ...inputRefs,
+      [name]: ref
+    })
+  };
+
+
+  const handleFormValuesChange = (name, value) => {
     setFormValues({
       ...formValues,
       [name]: value,
     });
   };
 
+  const getRefValue = (ref) => {
+    if(ref && ref.current && ref.current.dataset.iata) {
+      return ref.current.dataset.iata;
+    }
+
+    return false;
+  }
+
   const onFormSubmit = (event) => {
-    const fromIataCode = document.querySelector("#autocomplete-airport-1")
-      .dataset.iata;
-    const toIataCode = document.querySelector("#autocomplete-airport-2").dataset
-      .iata;
     event.preventDefault();
+
+    // const fromIataCode = document.querySelector("#autocomplete-airport-1")
+    //   .dataset.iata;
+    // const toIataCode = document.querySelector("#autocomplete-airport-2").dataset
+    //   .iata;
+    
     if (onSubmit) {
-      console.log(refValues);
-      onSubmit(
-        formValues,
-        (formValues.from = fromIataCode),
-        (formValues.to = toIataCode)
-      );
+
+      const valuesToSend = {
+        ...formValues,
+        from: getRefValue(inputRefs.from) || formValues.from,
+        to: getRefValue(inputRefs.to) || formValues.to
+      };
+
+      //console.log('form values', valuesToSend);
+      
+      onSubmit(valuesToSend);
     }
   };
+
   return (
     <div className="search-bar__container">
+      <Helmet>
+        <script>
+          AirportInput("autocomplete-airport-1");
+          AirportInput("autocomplete-airport-2");
+        </script>
+      </Helmet>
       <form onSubmit={onFormSubmit} className="search-bar__form">
-        <div class="search-bar__input-container">
-          <div class="search-bar__input-text">
+        <div className="search-bar__input-container">
+          <div className="search-bar__input-text">
             <Input
               label="From"
               name="from"
               type="text"
               handleFieldChange={handleFormValuesChange}
               className="From"
-              dataIata="A"
               id="autocomplete-airport-1"
+
+              registerRef={registerRef}
             />
 
             <Input
@@ -54,17 +84,17 @@ const SearchBar = ({ onSubmit }) => {
               type="text"
               handleFieldChange={handleFormValuesChange}
               className="To"
-              dataIata="B"
               id="autocomplete-airport-2"
+
+              registerRef={registerRef}
             />
           </div>
-          <div class="search-bar__input-date">
+          <div className="search-bar__input-date">
             <Input
               label="Depart"
               name="depart"
               type="date"
               handleFieldChange={handleFormValuesChange}
-              dataIata="C"
               className="Depart"
             />
 
@@ -73,13 +103,12 @@ const SearchBar = ({ onSubmit }) => {
               name="back"
               type="date"
               handleFieldChange={handleFormValuesChange}
-              dataIata="D"
               className="Return"
             />
           </div>
         </div>
         <button className="search-bar__btn">
-          Search Flights<i class="fas fa-arrow-right"></i>
+          Search Flights<i className="fas fa-arrow-right"></i>
         </button>
       </form>
     </div>
