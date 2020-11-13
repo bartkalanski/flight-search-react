3ff26e3873ac3fef03b02835fedf9c858aebac76
Bart Kalanski <bkalanski@gmail.com>
2020-11-13 13:53:50 +0000

    Added tests to FlightResults component

--- src/components/FlightResults.js
+++ src/components/FlightResults.js
@@ -5,55 +5,58 @@ import ErrorMessage from "./ErrorMessage";
 import "./FlightItem/flightitem.css";
 
 const FlightResults = ({ results }) => {
-  if (results !== undefined) {
-    // Check whether form has been submitted
-    console.log(results)
+  if (results) {
+    if (results !== undefined) {
+      // Check whether form has been submitted
 
-    if (results.Quotes !== undefined) {
-      // User submitted form and API request has been successful
+      if (results.Quotes !== undefined) {
+        // User submitted form and API request has been successful
 
-      if (results.Quotes[0] !== undefined) {
-        // Flights available for specified date
-        return (
-          <div className="results__section">
-            <FlightItem
-              results={results}
-              outbound="Outbound"
-              date={results.Quotes[0].OutboundLeg.DepartureDate}
-              departure={results.Places[1].IataCode}
-              arrival={results.Places[0].IataCode}
+        if (results.Quotes[0] !== undefined) {
+          // Flights available for specified date
+          return (
+            <div className="results__section">
+              <FlightItem
+                results={results}
+                outbound="Outbound"
+                date={results.Quotes[0].OutboundLeg.DepartureDate}
+                departure={results.Places[1].IataCode}
+                arrival={results.Places[0].IataCode}
+              />
+              <FlightItem
+                results={results}
+                outbound="Inbound"
+                date={results.Quotes[0].InboundLeg.DepartureDate}
+                departure={results.Places[0].IataCode}
+                arrival={results.Places[1].IataCode}
+              />
+            </div>
+          );
+        } else {
+          // No flights available for specified date
+          return (
+            <ErrorMessage
+              errorTop="We're sorry we haven't got any flights available on that date"
+              errorBottom="Please pick another date"
             />
-            <FlightItem
-              results={results}
-              outbound="Inbound"
-              date={results.Quotes[0].InboundLeg.DepartureDate}
-              departure={results.Places[0].IataCode}
-              arrival={results.Places[1].IataCode}
-            />
-          </div>
-        );
+          );
+        }
       } else {
-        // No flights available for specified date
-        return (
-          <ErrorMessage
-            errorTop="We're sorry we haven't got any flights available on that date"
-            errorBottom="Please pick another date"
-          />
-        );
+        // User hasn't submitted the form yet
+        return <React.Fragment></React.Fragment>;
       }
     } else {
-      // User hasn't submitted the form yet
-      return <React.Fragment></React.Fragment>;
+      // User input wrong airport code
+      return (
+        <ErrorMessage
+          errorTop="We're sorry, our input only accepts IATA airport codes"
+          errorBottom="Please enter IATA airport code e.g JFK"
+        />
+      );
     }
   } else {
-    // User input wrong airport code
-    return (
-      <ErrorMessage
-        errorTop="We're sorry, our input only accepts IATA airport codes"
-        errorBottom="Please enter IATA airport code e.g JFK"
-      />
-    );
+    return <React.Fragment />;
   }
 };
 export default FlightResults;
-/* .results.Quotes[0] if(props.results.length > 0){*/
+
--- src/components/FlightResults.test.js
+++ src/components/FlightResults.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import FlightResults from "./FlightResults";
 import FlightItem from "./FlightItem/FlightItem";
 
@@ -12,7 +12,56 @@ describe("FlightResults", () => {
   it("should render without throwing an error", () => {
     expect(wrapper).not.toBeNull();
   });
-  it("should render FlightItem component", () => {
-    expect(wrapper.find(FlightItem)).toHaveLength(0);
+  it("should render React Fragment if no results available", () => {
+    const fragment = <React.Fragment />;
+    expect(wrapper.containsMatchingElement(fragment)).toBe(true);
+  });
+  it("should not render FlightItem if no results available", () => {
+    const item = <FlightItem />;
+    expect(wrapper.containsMatchingElement(item)).toBe(false);
+  });
+  it("should render error message if no flights available", () => {
+    const wrap = mount(<FlightResults results={{ Quotes: [] }} />);
+    const errorMessage = wrap.find(".error__container");
+    expect(errorMessage.length).toBe(1);
+  });
+
+  it("should render FlightItem if results available", () => {
+    const wrapper = shallow(
+      <FlightResults
+        results={{
+          Carriers: [
+            {
+              Name: "KLM",
+            },
+          ],
+          Places: [
+            {
+              Name: "Amsterdam",
+              IataCode: "AMS",
+            },
+            {
+              IataCode: "MAN",
+            },
+          ],
+          Quotes: [
+            {
+              MinPrice: "80",
+
+              Direct: "Not Direct",
+
+              OutboundLeg: {
+                DepartureDate: "20/11/2020",
+              },
+              InboundLeg: {
+                DepartureDate: "26/11/2020",
+              },
+            },
+          ],
+        }}
+      />
+    );
+    const item = <FlightItem />;
+    expect(wrapper.containsMatchingElement(item)).toBe(true);
   });
 });
